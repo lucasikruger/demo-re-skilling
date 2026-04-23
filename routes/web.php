743ac8753ef45f1dcade36fccbb0dfb8751fd271
lkruger <lucasikruger@gmail.com>
2026-04-22 21:40:36 -0300

    second iter

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\ReportDownloadController;
 use App\Http\Controllers\SpaController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/up', fn () => response()->json(['ok' => true]));
 Route::get('/reportes/{interviewSession}/pdf', ReportDownloadController::class);
 Route::get('/{any?}', SpaController::class)->where('any', '^(?!api|storage|up).*$');
